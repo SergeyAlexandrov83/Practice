d2e0f97a61f76ed60668c3a1319bda176e77d4b4
Sergey Alexandrov <residx83@gmail.com>
2022-11-22 04:55:28 +0300

    Task 2 Done

--- Feel like intern/Task 1/Program.cs
+++ Feel like intern/Task 1/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿Console.WriteLine("Программа выведет квадрат введенного числа.");
+Console.WriteLine("Введите число: ");
+Double number = Convert.ToDouble(Console.ReadLine());
+number *= number;
+Console.WriteLine("Квадрат числа равняется: ");
+Console.WriteLine(number);
