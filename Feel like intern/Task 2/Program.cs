1f821f7008a111aa44d959d510eaeb299a94c9c1
Sergey Alexandrov <residx83@gmail.com>
2022-11-22 05:38:06 +0300

    Some changes

--- Feel like intern/Task 2/Program.cs
+++ Feel like intern/Task 2/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Программа покажет большее и меньшее из двух введенных чисел.");
-Console.WriteLine("Введите превое число: ");
+Console.WriteLine("Введите превое число, a = ");
 Double number_one = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
+Console.WriteLine("Введите второе число, b = ");
 Double number_two = Convert.ToDouble(Console.ReadLine());
 if (number_one < number_two) 
 {
@@ -9,11 +9,19 @@ if (number_one < number_two)
     Console.Write(number_one);
     Console.Write(" меньше числа ");
     Console.Write(number_two);
+    Console.Write("max = ");
+    Console.Write(number_two;
+    Console.Write(", min = ");
+    Console.Write(number_one);
 }
 else
 {
     Console.Write("Число ");
     Console.Write(number_one);
     Console.Write(" больше числа ");
+    Console.Write(number_two);
+    Console.Write("max = ");
+    Console.Write(number_one;
+    Console.Write(", min = ");
     Console.Write(number_two);
 }
