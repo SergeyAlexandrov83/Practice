7d1117ae85a999483c8d7ebc0c7985bbfa34207e
Sergey Alexandrov <residx83@gmail.com>
2022-11-22 05:06:54 +0300

    Task 3 Done

--- Feel like intern/Task 3/Program.cs
+++ Feel like intern/Task 3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Программа покажет большее и меньшее из двух введенных чисел.");
+Console.WriteLine("Введите превое число: ");
+Double number_one = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+Double number_two = Convert.ToDouble(Console.ReadLine());
+if (number_one < number_two) 
+{
+    Console.Write("Число ");
+    Console.Write(number_one);
+    Console.Write(" меньше числа ");
+    Console.Write(number_two);
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(number_one);
+    Console.Write(" больше числа ");
+    Console.Write(number_two);
+}
