1fd295683a8d799d6471462222f5b335d01d938a
Sergey Alexandrov <residx83@gmail.com>
2022-11-24 22:18:58 +0300

    Code changes

--- Feel like intern/Task 11/Program.cs
+++ Feel like intern/Task 11/Program.cs
@@ -1,20 +1,22 @@
 ﻿Console.WriteLine("Программа покажет максимальную цифру двухзначного числа.");
-Console.WriteLine("Введите число в диапазоне [10..99]: ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number >= 10 && number < 100 )
+// Console.WriteLine("Введите число в диапазоне [10..99]: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+int number = new Random().Next(10,100);
+Console.WriteLine(number);
+// if (number >= 10 && number < 100 )
+// {
+int numberFirst = number / 10;
+int numberSecond = number % 10;
+if (numberFirst < numberSecond)
 {
-    int numberFirst = number / 10;
-    int numberSecond = number % 10;
-    if (numberFirst < numberSecond)
-    {
-        Console.WriteLine(numberSecond);
-    }
-    else
-    {
-        Console.WriteLine(numberFirst);
-    }
+    Console.WriteLine(numberSecond);
 }
 else
 {
-    Console.WriteLine("Введенное число не двухзначное!");
+    Console.WriteLine(numberFirst);
 }
+// }
+// else
+// {
+//     Console.WriteLine("Введенное число не двухзначное!");
+// }
--- Feel like intern/Task 12/Program.cs
+++ Feel like intern/Task 12/Program.cs
@@ -1,13 +1,15 @@
 ﻿Console.WriteLine("Программа удалит вторую цифру трехзначного числа.");
-Console.WriteLine("Введите число в диапазоне [100..999]: ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number >= 100 && number < 1000 )
-{
-    int numberFirst = number / 100;
-    int numberThird = number % 10;
-    Console.WriteLine($"Удалив из числа {number} вторую цифру получим число {numberFirst * 10 + numberThird}");
-}
-else
-{
-    Console.WriteLine("Введенное число не трехзначное!");
-}
+// Console.WriteLine("Введите число в диапазоне [100..999]: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+// if (number >= 100 && number < 1000 )
+// {
+int numberFirst = number / 100;
+int numberThird = number % 10;
+Console.WriteLine($"Удалив из числа {number} вторую цифру получим число {numberFirst * 10 + numberThird}");
+// }
+// else
+// {
+//     Console.WriteLine("Введенное число не трехзначное!");
+// }
