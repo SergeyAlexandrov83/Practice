c3ff325a5558f8b23075a729b351353cbddad8a8
Sergey Alexandrov <residx83@gmail.com>
2022-11-25 00:55:52 +0300

    Dir Tree Change

---
+++ HW_Tasks/Task 16_HW_Seminar_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Программа покажет название дня недели восле введения числа.");
+Console.WriteLine("Введите число: ");
+Double number = Convert.ToDouble(Console.ReadLine());
+if (number > 0 && number < 6) 
+{
+    Console.WriteLine("Иди работать!");
+}
+else if (number == 6) 
+{
+    Console.WriteLine("Суббота - а суббота - выходной!");
+}
+else if (number == 7) 
+{
+    Console.WriteLine("Воскресенье - а воскресенье - выходной!");
+}
+else
+{
+    Console.WriteLine("Такого дня недели не существует, поэтому иди работать!");
+}
---
+++ Tasks/Task 17/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Программа ответит является ли первое введенное число квадратом второго введенного числа.");
+Console.WriteLine("Введите превое число: ");
+double number_one = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double number_two = Convert.ToDouble(Console.ReadLine());
+// Variant 1
+// if (number_one * number_one == number_two || number_two * number_two == number_one) 
+// {
+//     Console.Write($"Да, является!");
+// }
+// else
+// {
+//     Console.Write($"Нет, не является!");
+// }
+// Variant 2
+if (number_two * number_two == number_one) 
+{
+    Console.Write($"Число {number_one} является квадратом числа {number_two}");
+}
+else
+{
+    Console.Write($"Число {number_one} не является квадратом числа {number_two}");
+}
+if (number_one * number_one == number_two) 
+{
+    Console.Write($"Число {number_two} является квадратом числа {number_one}");
+}
+else
+{
+    Console.Write($"Число {number_two} не является квадратом числа {number_one}");
+}
