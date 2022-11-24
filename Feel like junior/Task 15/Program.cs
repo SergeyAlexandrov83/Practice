e966a42ed4d6e4be494bfb1b8165605836feadd3
Sergey Alexandrov <residx83@gmail.com>
2022-11-25 00:29:30 +0300

    Work field added

--- Feel like junior/Task 15/Program.cs
+++ Feel like junior/Task 15/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Программа проверит кратно ли введенное число 7 и 23.");
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 ==0)
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23!");
+}
+else
+{
+    Console.WriteLine($"Число {number} не кратно одновременно 7 и 23!");
+}
