1c494deea8a484af55438c50029f31da974ecd3d
Sergey Alexandrov <residx83@gmail.com>
2022-11-25 03:03:26 +0300

    IDK easyer? but less strings xD

--- Tasks/Task 22/Program.cs
+++ Tasks/Task 22/Program.cs
@@ -1,32 +1,25 @@
-﻿double coorddelta(double n1, double n2) {
-    return Math.Abs(n1 - n2);
-}
-double sqredelta(double n1) {
-    return n1 * n1;
+﻿double input() {
+    Console.WriteLine("Введите первую координату: ");
+    double coord1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите вторую координату: ");
+    double coord2 = Convert.ToDouble(Console.ReadLine());
+    double lineDeltaPow2 = Math.Pow(Math.Abs(coord1 - coord2), 2);
+    return lineDeltaPow2;
 }
 
 Console.WriteLine("Программа найдет длину отрезка между точками в пространстве 2D/3D!");
 Console.WriteLine("Введите '2' если работаем в 2D и '3' если работаем в 3D: ");
 int workField = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine("Введите первую координату X: ");
-double X1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите вторую координату X: ");
-double X2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите первую координату Y: ");
-double Y1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите вторую координату Y: ");
-double Y2 = Convert.ToDouble(Console.ReadLine());
-double sqredeltaX = sqredelta(coorddelta(X1, X2));
-double sqredeltaY = sqredelta(coorddelta(Y1, Y2));
-double XY = Math.Sqrt(sqredeltaX+sqredeltaY);
+Console.WriteLine("Введите координаты по оси X: ");
+double firstCoordDeltaPow2 = input();
+Console.WriteLine("Введите координаты по оси Y: ");
+double secondCoordDeltaPow2 = input();
+double XY = Math.Sqrt(firstCoordDeltaPow2+secondCoordDeltaPow2);
 if (workField == 3)
 {
-    Console.WriteLine("Введите первую координату Z: ");
-    double Z1 = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("Введите вторую координату Z: ");
-    double Z2 = Convert.ToDouble(Console.ReadLine());
-    double sqredeltaZ = sqredelta(coorddelta(Z1, Z2));
-    double XYZ = Math.Sqrt(sqredeltaX+sqredeltaY+sqredeltaZ);
+    Console.WriteLine("Введите координаты по оси Z: ");
+    double thirdCoordDeltaPow2 = input();
+    double XYZ = Math.Sqrt(firstCoordDeltaPow2+secondCoordDeltaPow2+thirdCoordDeltaPow2);
     Console.WriteLine($"Длинна отрезка = {XYZ}");
 }
 else
