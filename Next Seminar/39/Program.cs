048b56f8c7c8a9ff35c453166e662f719ce8c4e3
Sergey Alexandrov <residx83@gmail.com>
2022-12-12 02:26:40 +0300

    Start task 39

--- Next Seminar/39/Program.cs
+++ Next Seminar/39/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] GetRandomArray(int size, int leftRange = 0, int rightRange = 50)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange +1);
+    }
+
+    return array;
+}
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temp;
+    }
+}
+
+const int SIZE = 8;
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 50;
+
+int[] arr = GetRandomArray(SIZE, LEFTRANGE, RIGHTRANGE);
+Console.WriteLine(string.Join(", ", arr));
+
+ReverseArray(arr);
+Console.WriteLine();
+Console.WriteLine(string.Join(", ", arr));
--- Next Seminar/40/Program.cs
+++ Next Seminar/40/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 3 числа - которые соотвктствуют длинам сторон треугольника: ");
+double AB = Convert.ToDouble(Console.ReadLine());
+double BC = Convert.ToDouble(Console.ReadLine());
+double CA = Convert.ToDouble(Console.ReadLine());
